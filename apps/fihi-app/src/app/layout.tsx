f9edf5af3c0b76ee4cdec38035a9a9d36b4c39a3
uma-uc <uma.apple123@gmail.com>
2024-04-26 10:22:26 -0700

    feat: reset

--- apps/fihi-app/src/app/layout.tsx
+++ apps/fihi-app/src/app/layout.tsx
@@ -1,20 +1,8 @@
 'use client';
-import type { SupportedLanguages  } from "@incmix/i18n";
-; // Add this line to import the missing 'pageHeader' function
-
-export const supportedLangs: SupportedLanguages = {
-  // @ts-ignore
-  en: "English",
-  ar: "Arabic (العربية)",
-};
 
 function App() {
-  // config({supportedLangs, defaultLang: "en"});
-  
-
-  return (
-   </>
-  );
+  // config({supportedLangs, defaultLang: "en"}); 
+  return <div/>
 }
 
 export default App;
