f3bfdafa8d9f86ae44dcfa39a1785dba9173d04b
uma-uc <uma.apple123@gmail.com>
2024-04-26 11:15:43 -0700

    feat: layer-tailwind in radix

--- apps/fihi-app/src/app/layout.tsx
+++ apps/fihi-app/src/app/layout.tsx
@@ -1,8 +1,16 @@
 'use client';
+import "@incmix/ui/styles/global.css"
+import {Flex, Button, Text, Theme} from "@incmix/ui";
 
 function App() {
-  // config({supportedLangs, defaultLang: "en"}); 
-  return <div/>
+  return (
+    <Theme>
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes :)</Text>
+        <Button className="bg-red-300">Red Tailwind Button</Button>
+        <Button label="Ghost" variant="ghost"></Button>
+      </Flex>
+    </Theme>
+  );
 }
-
 export default App;
--- frontend/core/ui/index.tsx
+++ frontend/core/ui/index.tsx
@@ -2,3 +2,5 @@
 export {Theme} from "@radix-ui/themes"
 export {Box, Flex, Container, Section, Grid, Text} from "@radix-ui/themes"
 export { cn } from "./lib/utils"
+export { Button } from "./components/ui/button"
+export type { ButtonProps } from "./components/ui/button"
